fde66f5c6c60f97d53f7d8541084899b8eb2b9ad
m.elmostafa <m.elmostafa@uniicomg.com>
2020-11-23 16:36:07 +0200

    add screens

---
+++ app/src/main/java/com/unicom/wasalakcourier/ui/splash/SplashScreenFragment.kt
@@ -0,0 +1,22 @@
+package com.unicom.wasalakcourier.ui.splash
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.unicom.wasalakcourier.R
+
+class SplashScreenFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+    }
+
+    companion object {
+
+    }
+}
